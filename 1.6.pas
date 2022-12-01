e85b8be64fa9f22e373660c0f26476db6ae0768f
ilushka <117807745+IlyaKokovihin@users.noreply.github.com>
2022-12-01 14:49:20 +0300

    Add files via upload

---
+++ 1.6.pas
@@ -0,0 +1,12 @@
+﻿var masa: array [1..20] of integer;
+    i:integer;
+    upor: boolean;
+begin 
+  for i:= 1 to 20 do
+    readln(masa[i]);
+  for i:= 2 to 20 do
+    if masa[i] < masa[i - 1] then
+      upor:= True;
+    if upor = False then writeln ('Упорядочен'); 
+    if upor = True then writeln ('не упорядочен');
+ end.
